c66035f2801ac07678ab6bb2dec54e84df695b97
JuanPalms <111150185+JuanPalms@users.noreply.github.com>
2022-12-09 13:28:23 -0600

    muevo funcion delaunay a utils1.py

--- src/utils.py
+++ src/utils.py
@@ -9,6 +9,7 @@ import numpy as np
 from numpy import linalg as LA
 import warnings
 import math
+from utils1 import delaunay
 
 warnings.filterwarnings("ignore")
 #funcion auxiliar para calcular errores relativos
@@ -56,42 +57,9 @@ def RK4(f,a,b,alpha,N):
 
 #funcion que define a la ecuacion diferencial que resolveremos
 def fun_difEq(x,t):
-    np.sqrt((4*x**2-(lam*x**2-k)**2))/(lam*x**2-k):
+    np.sqrt((4*x**2-(lam*x**2-k)**2))/(lam*x**2-k)
+
+    
 
 
-#La siguiente funcion es una especie de grid search para buscar los mejores hiperparametros
-# de la ecuacion diferencial que nos permitan aproximar de mejor manera la solucion del problema
-#de optimizacion
 
-def Delaunay(k1,lambda,a,b,xa,xb,vol):
-    '''
-    comentar esta funcion
-    '''
-    for (i in 1:n):
-        for (j in 1:m):
-            lam =lambda[j]
-            k=k1[i]
-            fun = fun_difEq #funcion de la ecuacion diferencial que resolveremos
-
-            solu=RK4(fun,a, b, alpha=xa, N=1000)[1]
-            final=length(solu)
-            #verificar que el vector de soluciones no tenga nan's
-            if(solu[final]!=’nan’):
-                #verificar que la condicion final e inicial no sean iguales
-                if(xb!=xa):
-                    xb=1195.5
-                    error=abs(solu[final]-xb) #verificamos que el ultimo punto de la solucion este cerca de la condicion final
-                    #si cierto, nos quedamos con esta solucion y verificamos que cumple la condicion de volumen
-                    if(error<0.4):
-                        areas=rep(0,length(solu))
-                        fun = fun_difEq
-
-                        #Verificar que la solucion satisface la condición de volumen
-                        integrando=pi*solu^2
-                        for(i in 2:(length(solu)+1)):
-                            areas[i-1]=0.001*(integrando[i]+integrando[i-1])/2
-
-                        integral=sum(areas[1:length(areas)-1])
-
-                        #calcular error de la integral
-                        err_integral=abs(integral-vol)
